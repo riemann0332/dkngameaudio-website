729f413dc24121367b028206d36a9915767d487f
riemann0332 <davidecaldarone2@gmail.com>
2026-06-04 09:10:23 +0200

    Make layout responsive on mobile

--- scripts/cards.js
+++ scripts/cards.js
@@ -2,10 +2,22 @@ const dataTargets = document.querySelectorAll("div[data-target]");
 
 for (let element of dataTargets) {
     element.addEventListener('click', () => {
+        const rect = element.getBoundingClientRect();
+        if (rect.left < 0 || rect.right > window.innerWidth) {
+            element.scrollIntoView();
+            return;
+        }
+
+        element.classList.add('card-selected');
         const current = document.querySelector(".is-active");
-        current.classList.remove("is-active");
         const sectionId = element.dataset.target;
         const targetSection = document.getElementById(sectionId);
-        targetSection.classList.add('is-active');
+        setTimeout(
+            () => {
+                current.classList.remove("is-active");
+                targetSection.classList.add('is-active')
+            }, 350,
+        );
+        
     })
 }
